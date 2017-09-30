5e1d27d8b6ffd7cc0b0fc3eb633d6041421751d3
te1575@att.com <te1575@att.com>
2017-09-29 17:11:22 -0700

    Added more models

---
+++ src/main/java/com/att/eg/common/platform/kubekleaner/model/AutoscalerSpec.java
@@ -0,0 +1,71 @@
+/*
+
+MIT License (MIT)
+
+Copyright (c) 2017 AT&T Intellectual Property. All other rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a 
+copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the 
+Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included 
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+THE SOFTWARE.
+
+*/
+
+package com.att.eg.common.platform.kubekleaner.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class AutoscalerResource {
+	private String apiVersion;
+	private String kind;
+	private Metadata metadata;
+	private AutoscalerSpec spec;
+	
+	public AutoscalerResource export(){
+		if(this.getMetadata() != null){
+			this.setMetadata(metadata.export());
+		}
+		return this;
+	}
+	
+	public String getApiVersion() {
+		return apiVersion;
+	}
+	public void setApiVersion(String apiVersion) {
+		this.apiVersion = apiVersion;
+	}
+	public String getKind() {
+		return kind;
+	}
+	public void setKind(String kind) {
+		this.kind = kind;
+	}
+	public Metadata getMetadata() {
+		return metadata;
+	}
+	public void setMetadata(Metadata metadata) {
+		this.metadata = metadata;
+	}
+	public AutoscalerSpec getSpec() {
+		return spec;
+	}
+	public void setSpec(AutoscalerSpec spec) {
+		this.spec = spec;
+	}
+	
+}
